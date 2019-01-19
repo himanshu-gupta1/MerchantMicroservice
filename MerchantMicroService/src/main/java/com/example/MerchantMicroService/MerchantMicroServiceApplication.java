829815ed1363ff2e3eccb9ce23fc225e52a7c7c3
Himanshu Gupta <himanshugupta@Himanshus-MacBook-Pro.local>
2019-01-19 14:28:32 +0530

    inital commit

---
+++ MerchantMicroService/src/main/java/com/example/MerchantMicroService/MerchantMicroServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.MerchantMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantMicroServiceApplication.class, args);
+	}
+
+}
+
